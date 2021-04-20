d77feccbb275b48925e0f75143a0226cf287509b
SSZX866 <2386316625@qq.com>
2021-04-20 19:54:25 +0800

    2020.4.20

--- 堆栈/2.middle/313. 超级丑数.py
+++ 堆栈/2.middle/313. 超级丑数.py
@@ -2,7 +2,23 @@
 # @Time    : 2021/4/20 16:34
 # @File    : 313. 超级丑数.py
 from leetcode import *
+
+
 class Solution:
     def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
-        heap = []
-        
+        heap, dic = [1], set()
+        while n > 1:
+            ans = heapq.heappop(heap)
+            for prime in primes:
+                tmp = ans * prime
+                if tmp not in dic:
+                    dic.add(tmp)
+                    heapq.heappush(heap, tmp)
+            n -= 1
+        return heapq.heappop(heap)
+
+
+if __name__ == '__main__':
+    n = 1
+    primes = [2, 7, 13, 19]
+    print(Solution().nthSuperUglyNumber(n, primes))
