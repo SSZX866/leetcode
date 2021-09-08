d58cafeff4bd33ca83d9a7f6a31ed3dfdea8d659
SSZX866 <2386316625@qq.com>
2021-09-08 11:36:52 +0800

    2021.9.8

---
+++ 堆栈/3.hard/502. IPO.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/8 09:59
+# @File    : 502. IPO.py
+from leetcode import *
+
+
+class HeapNode:
+    def __init__(self):
+        self.val = []
+        self.next = None
+
+
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        dummy = HeapNode()
+        dummy.next = HeapNode()
+        for i in range(len(profits)):
+            heapq.heappush(dummy.next.val, (-profits[i], capital[i]))
+        while k:
+            cur = dummy
+            flag = False
+            while not flag and cur.next:
+                tmp = HeapNode()
+                while cur.next.val:
+                    profit, cap = heapq.heappop(cur.next.val)
+                    print(-profit, cap, w)
+                    # time.sleep(0.05)
+                    if cap > w:
+                        heapq.heappush(tmp.val, (profit, cap))
+                    else:
+                        k -= 1
+                        w += -profit
+                        flag = not flag
+                        # 如果当前节点已空则删除当前节点
+                        tmp.next = cur.next if cur.next.val else cur.next.next
+                        cur.next = tmp
+                        break
+                if not flag:
+                    # 如果未完成则当前节点一定已空
+                    tmp.next = cur.next.next
+                    cur.next = tmp
+                    cur = tmp
+                    if not cur: break
+            if not flag and not cur: break
+        return w
+
+
+# heap中只存可以启动的项目（即capital<w）每次项目结束后更新heap
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        prof = sorted(zip(profits, capital), key=lambda x: x[1])
+        heap = []
+        idx, n = 0, len(profits)
+        while k:
+            while idx < n and prof[idx][1] <= w:
+                profit, cap = prof[idx]
+                heapq.heappush(heap, (-profit, cap))
+                idx += 1
+            if heap:
+                w += -heapq.heappop(heap)[0]
+                k -= 1
+            else:
+                break
+        return w
+
+
+if __name__ == '__main__':
+    k = 2
+    w = 0
+    profits = [1, 2, 3]
+    capital = [0, 1, 1]
+    print(Solution().findMaximizedCapital(k, w, profits, capital))
